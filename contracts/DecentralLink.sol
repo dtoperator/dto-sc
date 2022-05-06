e88a4303abe24ac11c619c33b6075c3887de50aa
GiperMeon <gnatoffskiy43@gmail.com>
2022-05-06 15:39:13 +0700

    add setMaxSizePrefix

--- contracts/DecentralLink.sol
+++ contracts/DecentralLink.sol
@@ -10,28 +10,30 @@ import "./interface/IDecentralLink.sol";
 contract DecentralLink is ERC721Enumerable, Ownable, IDecentralLink {
     using Strings for uint256;
 
-        // Base URI
+    // Base URI
     string private _uri;
-    
+
     uint256 public counter = 10000000;
+    uint256 public maxSizePrefix = 10;
 
     uint256 private _salePrice = 100 ether;
 
-    mapping (uint256 => uint256) public prefixPrice;
-    mapping (uint256 => address) public prefixOwner;
-    mapping (uint256 => string) public prefixName;
-    mapping (string => uint256) public prefixId;
+    mapping(uint256 => uint256) public prefixPrice;
+    mapping(uint256 => address) public prefixOwner;
+    mapping(uint256 => string) public prefixName;
+    mapping(string => uint256) public prefixId;
 
     bool public pause;
 
     event AddPrefix(string prefix_, uint256 counter_, uint256 price_);
     event ChangePrice(uint256 id, uint256 price_);
     event MintNumber(uint256 prefixNumber);
-    
-    constructor(string memory name_, string memory symbol_) ERC721(name_, symbol_) {
-    }
 
-    modifier checkPause {
+    constructor(string memory name_, string memory symbol_)
+        ERC721(name_, symbol_)
+    {}
+
+    modifier checkPause() {
         require(pause, "Mint NFT paused");
         _;
     }
@@ -40,17 +42,29 @@ contract DecentralLink is ERC721Enumerable, Ownable, IDecentralLink {
         _uri = uri_;
     }
 
-    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
-        require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
-
-        
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        virtual
+        override
+        returns (string memory)
+    {
+        require(
+            _exists(tokenId),
+            "ERC721Metadata: URI query for nonexistent token"
+        );
 
         uint256 lenNumber = bytes(tokenId.toString()).length;
-        uint256 prefix_ = uint256(tokenId / 10 ** (lenNumber - 8));
-        uint256 number_ = tokenId % 10 ** (lenNumber - 8);
-        string memory baseURI =  string(abi.encodePacked(_baseURI(), prefixName[prefix_]));
+        uint256 prefix_ = uint256(tokenId / 10**(lenNumber - 8));
+        uint256 number_ = tokenId % 10**(lenNumber - 8);
+        string memory baseURI_ = string(
+            abi.encodePacked(_baseURI(), prefixName[prefix_])
+        );
 
-        return bytes(_baseURI()).length > 0 ? string(abi.encodePacked(baseURI, number_.toString())) : "";
+        return
+            bytes(_baseURI()).length > 0
+                ? string(abi.encodePacked(baseURI_, number_.toString()))
+                : "";
     }
 
     function _baseURI() internal view override returns (string memory) {
@@ -61,24 +75,29 @@ contract DecentralLink is ERC721Enumerable, Ownable, IDecentralLink {
         return _baseURI();
     }
 
-    function setPause (bool status_) override external {
+    function setPause(bool status_) external override {
         pause = status_;
     }
 
-    function setSalePrice (uint256 price) external onlyOwner {
+    function setSalePrice(uint256 price) external override onlyOwner {
         _salePrice = price;
     }
 
-    function _indexOf(string memory _base, string memory _value, uint _offset)
-        internal
-        pure
-        returns (bool) {
+    function setMaxSizePrefix(uint256 size) external override onlyOwner {
+        maxSizePrefix = size;
+    }
+
+    function _indexOf(
+        string memory _base,
+        string memory _value,
+        uint256 _offset
+    ) internal pure returns (bool) {
         bytes memory _baseBytes = bytes(_base);
         bytes memory _valueBytes = bytes(_value);
 
         require(_valueBytes.length == 1, "Error in indexOf");
 
-        for (uint i = _offset; i < _baseBytes.length; i++) {
+        for (uint256 i = _offset; i < _baseBytes.length; i++) {
             if (_baseBytes[i] == _valueBytes[0]) {
                 return false;
             }
@@ -87,9 +106,15 @@ contract DecentralLink is ERC721Enumerable, Ownable, IDecentralLink {
         return true;
     }
 
-    function _addPrefix(string memory prefix_, uint256 price) internal returns(uint256) {
+    function _addPrefix(string memory prefix_, uint256 price)
+        internal
+        returns (uint256)
+    {
         require(bytes(prefix_).length > 0, "Error: Empty string");
-        require(bytes(prefix_).length < 10, "Error: This prefix bigest");
+        require(
+            bytes(prefix_).length < maxSizePrefix,
+            "Error: This prefix bigest"
+        );
         require(_indexOf(prefix_, " ", 0), "Error: contains a space");
         require(prefixId[prefix_] < 100000000, "This prefix busy");
         require(counter < 100000000, "Error: end prefix counter");
@@ -106,50 +131,73 @@ contract DecentralLink is ERC721Enumerable, Ownable, IDecentralLink {
         return counter - 1;
     }
 
-    function addPrefixOwner(string memory prefix_, uint256 price) override onlyOwner checkPause external returns(uint256) {
+    function addPrefixOwner(string memory prefix_, uint256 price)
+        external
+        override
+        onlyOwner
+        checkPause
+        returns (uint256)
+    {
         return _addPrefix(prefix_, price);
     }
 
-    function addPrefix(string memory prefix_, uint256 price) payable override checkPause external returns(uint256) {
+    function addPrefix(string memory prefix_, uint256 price)
+        external
+        payable
+        override
+        checkPause
+        returns (uint256)
+    {
         uint256 id = _addPrefix(prefix_, price);
 
-        (bool success, ) = payable(owner()).call{ value: msg.value }("");
+        (bool success, ) = payable(owner()).call{value: msg.value}("");
 
         require(
-                success,
-                "Address: unable to send value, recipient may have reverted"
-            );
+            success,
+            "Address: unable to send value, recipient may have reverted"
+        );
 
         return id;
     }
-    
-    function changePrice(uint256 id, uint256 price) override checkPause external {
-        require( prefixOwner[id] == msg.sender, "Error: You aren`t owner this Prefix" );
+
+    function changePrice(uint256 id, uint256 price)
+        external
+        override
+        checkPause
+    {
+        require(
+            prefixOwner[id] == msg.sender,
+            "Error: You aren`t owner this Prefix"
+        );
         prefixPrice[id] = price;
         emit ChangePrice(id, price);
     }
 
-    function mintNumber(uint256 prefixNumber) override payable checkPause external {
-
+    function mintNumber(uint256 prefixNumber)
+        external
+        payable
+        override
+        checkPause
+    {
         uint256 lenNumber = bytes(prefixNumber.toString()).length;
         console.log(lenNumber);
-        uint256 prefix_ = prefixNumber / 10 ** (lenNumber - 8);
-        console.log(prefixNumber % 10 ** (lenNumber - 8));
+        uint256 prefix_ = prefixNumber / 10**(lenNumber - 8);
+        console.log(prefixNumber % 10**(lenNumber - 8));
 
         require(prefixOwner[prefix_] != address(0), "Error: incorrect prefix");
         require(lenNumber - 8 < 11, "Error: incorrect length number");
         require(msg.value >= prefixPrice[prefix_], "Error: incorrect price");
 
         _safeMint(msg.sender, prefixNumber);
-        
+
         emit MintNumber(prefixNumber);
 
-        (bool success, ) = payable(prefixOwner[prefix_]).call{ value: msg.value }("");
+        (bool success, ) = payable(prefixOwner[prefix_]).call{value: msg.value}(
+            ""
+        );
         require(
-                success,
-                "Address: unable to send value, recipient may have reverted"
-            );  
-
+            success,
+            "Address: unable to send value, recipient may have reverted"
+        );
     }
-
 }
--- contracts/interface/IDecentralLink.sol
+++ contracts/interface/IDecentralLink.sol
@@ -2,16 +2,22 @@
 pragma solidity ^0.8.0;
 
 interface IDecentralLink {
-    
     function setPause(bool status_) external;
 
-    function addPrefixOwner(string memory prefix_, uint256 price) external returns(uint256);
+    function setSalePrice(uint256 price) external;
 
-    function addPrefix(string memory prefix, uint256 price) payable external returns(uint256);
-       
+    function setMaxSizePrefix(uint256 size) external;
+
+    function addPrefixOwner(string memory prefix_, uint256 price)
+        external
+        returns (uint256);
+
+    function addPrefix(string memory prefix, uint256 price)
+        external
+        payable
+        returns (uint256);
+
     function changePrice(uint256 id, uint256 price) external;
 
-    function mintNumber(uint256 number) payable external;
-
-
+    function mintNumber(uint256 number) external payable;
 }
